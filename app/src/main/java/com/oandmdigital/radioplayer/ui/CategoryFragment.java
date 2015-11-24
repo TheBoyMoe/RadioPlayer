6c451388c62a26c47724916528e9a211d09e42dd
Bill Fero <billfero@gmail.com>
2015-11-24 19:26:54 +0000

    Implemented a ListView with custom list item and view holder pattern.

--- app/src/main/java/com/oandmdigital/radioplayer/ui/CategoryFragment.java
+++ app/src/main/java/com/oandmdigital/radioplayer/ui/CategoryFragment.java
@@ -1,14 +1,16 @@
 package com.oandmdigital.radioplayer.ui;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.oandmdigital.radioplayer.R;
 import com.oandmdigital.radioplayer.event.DownloadCategoriesEvent;
 import com.oandmdigital.radioplayer.event.PostCategoryEvent;
 import com.oandmdigital.radioplayer.model.Category;
@@ -18,7 +20,10 @@ import java.util.List;
 
 import de.greenrobot.event.EventBus;
 
-public class CategoryFragment extends ListFragment{
+public class CategoryFragment extends Fragment{
+
+    private ListView listview;
+    private CategoryAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,16 @@ public class CategoryFragment extends ListFragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        listview = (ListView) inflater.inflate(R.layout.list_view, container, false);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Category category = (Category) parent.getItemAtPosition(position);
+                EventBus.getDefault().post(new PostCategoryEvent(category));
+            }
+        });
+        return listview;
     }
 
 
@@ -49,36 +63,92 @@ public class CategoryFragment extends ListFragment{
     @SuppressWarnings("unused")
     public void onEventMainThread(DownloadCategoriesEvent event) {
         // bind the category list to the adapter and display
-        setListAdapter(new CategoryAdapter(event.getCategories()));
+        //setListAdapter(new CategoryAdapter(event.getCategories()));
+        listview.setAdapter(new CategoryAdapter(event.getCategories()));
     }
 
 
-    @Override
-    public void onListItemClick(ListView listview, View view, int position, long id) {
-        // retrieve the category object of the list item clicked on
-        Category category = ((CategoryAdapter)getListAdapter()).getItem(position);
 
-        // post the category to the eventbus, retrieve in the hosting activity
-        EventBus.getDefault().post(new PostCategoryEvent(category));
-    }
+//    @Override
+//    public void onListItemClick(ListView listview, View view, int position, long id) {
+//        // retrieve the category object of the list item clicked on
+//        Category category = ((CategoryAdapter)getListAdapter()).getItem(position);
+//
+//        // post the category to the eventbus, retrieve in the hosting activity
+//        EventBus.getDefault().post(new PostCategoryEvent(category));
+//    }
 
-    private class CategoryAdapter extends ArrayAdapter<Category> {
 
-        // using the default TextView layout to display the category
+
+    // basic ArrayAdapter which displays the category object through the default textview
+//    private class CategoryAdapter extends ArrayAdapter<Category> {
+//
+//        // using the default TextView layout to display the category
+//        public CategoryAdapter(List<Category> items) {
+//            super(getActivity(), android.R.layout.simple_list_item_1, items);
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//
+//            View view = super.getView(position, convertView, parent);
+//            TextView title = (TextView) view.findViewById(android.R.id.text1);
+//            title.setText(getItem(position).toString());
+//
+//            return view;
+//        }
+//    }
+
+
+    // ArrayAdapter which implements a custom list item and view holder pattern
+    private class CategoryAdapter extends ArrayAdapter<Category>{
+
+        // pass in the context and data set to the constructor
         public CategoryAdapter(List<Category> items) {
-            super(getActivity(), android.R.layout.simple_list_item_1, items);
+            super(getActivity(), 0, items);
         }
+
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            View view = super.getView(position, convertView, parent);
-            TextView title = (TextView) view.findViewById(android.R.id.text1);
-            title.setText(getItem(position).toString());
+            if(convertView == null)
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item, parent, false);
 
-            return view;
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            if(holder == null) {
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }
+
+            // populate the holder elements
+            Category item = getItem(position);
+            holder.name.setText(item.getTitle());
+
+            String description = item.getDescription();
+            if(description == null || description.isEmpty())
+                description = item.getSlug();
+            holder.description.setText(description);
+
+            return convertView;
         }
+
     }
+
+
+
+    private class ViewHolder {
+
+        TextView name;
+        TextView description;
+
+        public ViewHolder(View row) {
+            name = (TextView) row.findViewById(R.id.name);
+            description = (TextView) row.findViewById(R.id.description);
+        }
+
+    }
+
 
 
 }
